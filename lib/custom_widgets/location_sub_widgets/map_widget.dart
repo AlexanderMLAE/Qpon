f1f47555db77b53f7cab97276446b65b05822d4c
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2025-11-25 07:57:24 -0500

    mapa con ubicacion actual

--- lib/custom_widgets/location_sub_widgets/map_widget.dart
+++ lib/custom_widgets/location_sub_widgets/map_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
@@ -19,7 +20,10 @@ class _MapWidgetState extends State<MapWidget> {
         FlutterMap(
           mapController: _mapController,
           options: MapOptions(
-            initialCenter: LatLng(21.049454, -86.846441), // Coordenadas de la UT Cancun
+            initialCenter: LatLng(
+              21.049454,
+              -86.846441,
+            ), // Coordenadas de la UT Cancun
             initialZoom: 15.0,
             minZoom: 10.0,
             maxZoom: 100.0,
@@ -29,6 +33,7 @@ class _MapWidgetState extends State<MapWidget> {
             TileLayer(
               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             ),
+            CurrentLocationLayer(),
           ],
         ),
       ],
