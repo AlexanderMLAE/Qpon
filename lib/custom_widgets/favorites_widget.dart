7aa0f5af78ea524009836935b9c2dc3c8c8b8ecc
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-03-31 13:53:56 -0500

    Deleted unused import

--- lib/custom_widgets/favorites_widget.dart
+++ lib/custom_widgets/favorites_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_qpon/custom_widgets/sub_widgets/offer_card_builder.dart';
-import 'sub_widgets/offer_card_widget.dart';
 import 'sub_widgets/database_service.dart';
 
 class FavoritesWidget extends StatefulWidget {
