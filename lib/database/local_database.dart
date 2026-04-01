0a3f6cc85fb9a0d7400bdd5255a915ade051f29d
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-04-01 14:19:10 -0500

    Added function to delete all saved offers and added temporary buttons to delete and reload saved offers

--- lib/database/local_database.dart
+++ lib/database/local_database.dart
@@ -4,7 +4,7 @@ import 'package:proyecto_qpon/features/favorites/data/saved_offer_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LocalDatabase {
-  static Future<dynamic> getLocalDatabase() async {
+  static Future<Database> getLocalDatabase() async {
     WidgetsFlutterBinding.ensureInitialized();
     // open the database
     final database = openDatabase(
@@ -21,7 +21,7 @@ class LocalDatabase {
   }
 
   static Future<List<Map<String, Object?>>> getSavedOfferCards() async {
-    final db = await getLocalDatabase();
+    final Database db = await getLocalDatabase();
 
     final List<Map<String, Object?>> savedOfferCardMaps = await db.query(
       'favorite_offers',
@@ -31,7 +31,7 @@ class LocalDatabase {
   }
 
   static Future<void> insertSavedOfferCard(SavedOfferCard savedOffer) async {
-    final db = await getLocalDatabase();
+    final Database db = await getLocalDatabase();
 
     await db.insert(
       'favorite_offers',
@@ -39,5 +39,11 @@ class LocalDatabase {
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
     debugPrint('Oferta guardada en DB $savedOffer');
+  }
+
+  static Future<void> deleteSavedOfferCards() async {
+    final db = await getLocalDatabase();
+
+    await db.rawDelete('DELETE FROM favorite_offers');
   }
 }
