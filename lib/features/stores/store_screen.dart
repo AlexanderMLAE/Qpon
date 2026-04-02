cf3673d95a20b6e9b32f55dcf62c6c9b8ff953f9
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-04-02 07:52:20 -0500

    Fixed parameter issue

--- lib/features/stores/store_screen.dart
+++ lib/features/stores/store_screen.dart
@@ -22,7 +22,7 @@ class _StoreWidgetState extends State<StoreWidget> {
   Future<void> getOffers() async {
     String storeId = widget.storeData?["storeId"] as String;
     try {
-      final offers = await FirestoreService.getOffersList(storeId);
+      final offers = await FirestoreService.getOffersList(storeId: storeId);
       setState(() {
         _offers = offers;
       });
--- lib/shared/firestore_service.dart
+++ lib/shared/firestore_service.dart
@@ -22,7 +22,7 @@ class FirestoreService {
       return querySnapshot.docs.map((doc) {
         final id = doc.id;
         final data = doc.data() as Map<String, dynamic>;
-        debugPrint('doc id: $id');
+        debugPrint('fetched data: $id $data');
         return {'id': id, ...data};
       }).toList();
     } catch (e) {
@@ -32,11 +32,14 @@ class FirestoreService {
   }
 
   // Gets the list of offer maps and turns them to a list of offer objects
-  static Future<List<Offer>> getOffersList(String? storeId) async {
+  static Future<List<Offer>> getOffersList({
+    String? storeId,
+  }) async {
     List offersList = await _fetchOffers();
     if (storeId != null) {
       offersList = await _fetchOffers(storeId: storeId);
     }
+    debugPrint('offersList: $offersList ${offersList.map((map) => Offer.fromMapToOffer(map)).toList()}');
     return offersList.map((map) => Offer.fromMapToOffer(map)).toList();
   }
 
