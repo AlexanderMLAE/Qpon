ae9bb7707d48ff5b6e14e8d189cccfc2b48c575f
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-04-01 20:07:01 -0500

    reordered files appropiately

--- lib/features/offers/presentation/offer_card_widget.dart
+++ lib/features/offers/presentation/offer_card_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_qpon/features/offers/offer_details_screen.dart';
+import 'package:proyecto_qpon/features/offers/presentation/offer_details_screen.dart';
 
 class OfferCardWidget extends StatelessWidget {
   const OfferCardWidget({
