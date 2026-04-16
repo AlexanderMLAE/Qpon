453f7983c21d60a26ee6c982e58333e7f29d78fe
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-04-15 20:05:48 -0500

    Created lcoation class

---
+++ lib/features/stores/data/location_class.dart
@@ -0,0 +1,27 @@
+class Location {
+  // could be useful later but im commenting it out rn since im not using it for anything yet
+  final String id; /// Firestore ID
+  final String name;
+  final double long;
+  final double lat;
+
+  const Location({required this.name, required this.long, required this.lat, required this.id});
+
+  Map<String, Object?> toMap() {
+    return {'name': name, 'id': id, 'long': long, 'lat': lat};
+  }
+
+  @override
+  String toString() {
+    return "Location: name: $name - longitude: $long - latitude: $lat";
+  }
+
+  factory Location.fromMaptoLocation(Map<String, dynamic> locationMap) {
+    return Location(
+      name: locationMap['name'] as String? ?? 'Null name',
+      long: locationMap['long'] as double? ?? -86.84552, // Qpon HQ
+      lat: locationMap['lat'] as double? ?? 21.05021,
+      id: locationMap['id'] as String? ?? 'ID-Was-Null', // QPon HQ
+    );
+  }
+}
