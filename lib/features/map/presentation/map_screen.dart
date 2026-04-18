924279f255581980e4743bfdc456dd0ac8ce9e3d
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-04-17 22:24:14 -0500

    Moved files

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_qpon/features/map/custom_map_widget.dart';
+import 'package:proyecto_qpon/features/map/presentation/custom_map_widget.dart';
 import 'package:proyecto_qpon/shared/globals.dart';
 
 // Consider renaming some classes
